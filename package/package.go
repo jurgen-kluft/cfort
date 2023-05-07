1a8393ede50cec1742669c9834a44164a13f1ebc
jurgen.kluft <jurgen.kluft@gmail.com>
2023-05-07 18:29:25 +0800

    * cfort; table print

---
+++ package/package.go
@@ -0,0 +1,36 @@
+package cfort
+
+import (
+	cbase "github.com/jurgen-kluft/cbase/package"
+	denv "github.com/jurgen-kluft/ccode/denv"
+	ccore "github.com/jurgen-kluft/ccore/package"
+	cunittest "github.com/jurgen-kluft/cunittest/package"
+)
+
+// GetPackage returns the package object of 'cfort'
+func GetPackage() *denv.Package {
+	// Dependencies
+	cunittestpkg := cunittest.GetPackage()
+	cbasepkg := cbase.GetPackage()
+	ccorepkg := ccore.GetPackage()
+
+	// The main (cfort) package
+	mainpkg := denv.NewPackage("cfort")
+	mainpkg.AddPackage(cunittestpkg)
+	mainpkg.AddPackage(ccorepkg)
+
+	// 'cfort' library
+	mainlib := denv.SetupDefaultCppLibProject("cfort", "github.com\\jurgen-kluft\\cfort")
+	mainlib.Dependencies = append(mainlib.Dependencies, ccorepkg.GetMainLib())
+
+	// 'cfort' unittest project
+	maintest := denv.SetupDefaultCppTestProject("cfort_test", "github.com\\jurgen-kluft\\cfort")
+	maintest.Dependencies = append(maintest.Dependencies, cunittestpkg.GetMainLib())
+	maintest.Dependencies = append(maintest.Dependencies, cbasepkg.GetMainLib())
+	maintest.Dependencies = append(maintest.Dependencies, ccorepkg.GetMainLib())
+	maintest.Dependencies = append(maintest.Dependencies, mainlib)
+
+	mainpkg.AddMainLib(mainlib)
+	mainpkg.AddUnittest(maintest)
+	return mainpkg
+}
